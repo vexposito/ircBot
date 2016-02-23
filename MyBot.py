928c186e3dfbead211794434c91799d0e73670e3
Verónica Expósito Cano <v.exposito.cano@gmail.com>
2016-02-23 17:10:53 +0100

    Capturar mensajes
    En esta versión, el Bot es capaz de capturar los mensajes que recibe del
    canal y mostrarlos en la consola

--- MyBot.py
+++ MyBot.py
@@ -4,9 +4,15 @@ import socket
 import time
  
 servidor = "irc.irc-hispano.org"
-canal = "#IRCvero"
-nombre = "Elisa20"
+canal = "#irc-hispano"
+nombre = "Ane20"
 puerto = 6667
+HOST="irc.irc-hispano.net"
+PORT=6667
+NICK="Anne20"
+IDENT="Anne20"
+REALNAME="Anne"
+CHAN="#irc-hispano"
  
 def respuesta_ping(ircmsg, canal):
       if ircmsg.find("PING :") != -1:
@@ -32,6 +38,7 @@ def  obtener_nick(canal, ircmsg):
       if ircmsg.find("PRIVMSG "+canal) != -1:
             nick = ircmsg.split('!', 1 )
             nick = nick[0].replace(":", "",1)
+            print(ircmsg + canal)
             return nick
       
       
@@ -44,17 +51,22 @@ def obtener_mensaje(canal, ircmsg):
  
 def mostrar_chat(ircmsg, canal):
       print (obtener_nick(canal, ircmsg)+": "+obtener_mensaje(canal, ircmsg))
+
       
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((servidor, puerto))
 irc.send("USER "+ nombre +" "+ nombre +" "+ nombre +" \n\r")
 irc.send("NICK "+ nombre +"\n\r")
+irc.send("NICK %s\r\n" % NICK)
+irc.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+irc.send("JOIN :%s\r\n" % CHAN)
  
 print("Conectando... ")
 unirse_a_canal(canal)
 time.sleep(4)
 unirse_a_canal(canal)
 print("¡CONECTADO! ")
+
 #time.sleep(5)
 #desconectarse(canal)
 #print("¡Desconectado! ")
@@ -70,15 +82,23 @@ while 1:
       
       if ircmsg.find("PRIVMSG "+canal) != -1:
             mostrar_chat(ircmsg, canal)
+      
        
       if ircmsg.find("Hola") != -1:
             enviar_mensaje(canal, "Hola, soy Elisa")
+            ircmsg = ircmsg.strip('\n\r')
             
       if ircmsg.find("nombre") != -1:
             enviar_mensaje(canal, "Me llamo Elisa")
+           
 
       if ircmsg.find("edad") != -1:
             enviar_mensaje(canal, "20")
+
+      if ircmsg.find("edad") != -1:
+            obtener_mensaje(canal, ircms)
+
+
 
 
 
