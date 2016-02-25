7d78a9434c331eb9beb01ca053bc875d19fc2314
Verónica Expósito Cano <v.exposito.cano@gmail.com>
2016-02-25 12:21:11 +0100

    v0.4
    Captura mensajes desde el inicio, información de la sesión, capaz de
    escribir en el chat respuestas aleatorias que coge de una lista.

--- MyBot.py
+++ MyBot.py
@@ -1,18 +1,15 @@
 # -*- coding: cp1252 -*-
-#Comentario de vero
 import socket
 import time
+import string # módulo que contiene las secuencias comunes de caracteres ASCII
+import random # módulo que se ocupa de la generación aleatoria
  
-servidor = "irc.irc-hispano.org"
-canal = "#irc-hispano"
-nombre = "Ane20"
-puerto = 6667
-HOST="irc.irc-hispano.net"
-PORT=6667
-NICK="Anne20"
-IDENT="Anne20"
-REALNAME="Anne"
-CHAN="#irc-hispano"
+servidor    = "irc.irc-hispano.org"
+canal       = "#Granada"
+nombre      = "Aliiicia"
+puerto      = 6667
+ident       = "Aliiicia"
+realname    = "Aliiicia"
  
 def respuesta_ping(ircmsg, canal):
       if ircmsg.find("PING :") != -1:
@@ -52,53 +49,52 @@ def obtener_mensaje(canal, ircmsg):
 def mostrar_chat(ircmsg, canal):
       print (obtener_nick(canal, ircmsg)+": "+obtener_mensaje(canal, ircmsg))
 
+#Crear lista de n elementos
+
+lista = [ 'tea', 'noe', 'amo', 'oca', 'ley','oso', 'fea','hucha', 'ave','torre',
+         'teta','tina','tomo','taco','tela','tiza','tufo','techo','tubo','nuera',
+         'nido','niño','nomo','nuca','nilo','nuez','naife','nicho','nube','mar',
+         'mito','mono','mama','meca','mulo','mesa','mafia','mecha','mapa','corro',
+         'codo','caño','cama','coco','cola','cazo','cafe','coche','cubo','lira',
+         'loto','luna','lima','loco','lulu','lazo','lofio','lucha','lupa','suero',
+         'soda','ceño','suma','saco','sol','seso','sofa','acecha','sapo','faro',
+         'foto','fino','fama','foca','falo','fosa','fofo','ficha','fobia','choro',
+         'chuto','chino','chama','chico','chal','choza','chufa','chocho','chipo','pera',
+         'pito','pino','puma','boca','pala','peso','bofe','bache','pipa','torero']
+ 
+     
+
       
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((servidor, puerto))
 irc.send("USER "+ nombre +" "+ nombre +" "+ nombre +" \n\r")
 irc.send("NICK "+ nombre +"\n\r")
-irc.send("NICK %s\r\n" % NICK)
-irc.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
-irc.send("JOIN :%s\r\n" % CHAN)
+irc.send("NICK %s\r\n" % nombre)
+irc.send("USER %s %s bla :%s\r\n" % (ident, servidor, realname))
+irc.send("JOIN :%s\r\n" % canal)
  
-print("Conectando... ")
+print "Conectando... "+"\n\r"
 unirse_a_canal(canal)
 time.sleep(4)
 unirse_a_canal(canal)
-print("¡CONECTADO! ")
+print "¡CONECTADO! " +"\n\r"
+print "INFORMACIÓNDE LA SESION" +"\n\r" + time.strftime('%H:%M:%S / %d %b %y')+"\n\r"+ "Canal: " + canal + "\n\r"+ "Servidor: " + servidor + "\n\r"
+
 
 #time.sleep(5)
 #desconectarse(canal)
 #print("¡Desconectado! ")
 
- 
 while 1:
       ircmsg = irc.recv(512)
       
       if ((ircmsg.find("PRIVMSG") != -1) != 1):
             respuesta_ping(ircmsg,canal)
+            obtener_mensaje(canal, ircmsg)
+            print ircmsg  + "\n\r" 
+            #enviar_mensaje(canal, "Hola")
+            #enviar_mensaje(canal,random.choice(lista))
             
-      ircmsg = ircmsg.strip('\n\r')
-      
-      if ircmsg.find("PRIVMSG "+canal) != -1:
-            mostrar_chat(ircmsg, canal)
-      
-       
-      if ircmsg.find("Hola") != -1:
-            enviar_mensaje(canal, "Hola, soy Elisa")
             ircmsg = ircmsg.strip('\n\r')
-            
-      if ircmsg.find("nombre") != -1:
-            enviar_mensaje(canal, "Me llamo Elisa")
-           
-
-      if ircmsg.find("edad") != -1:
-            enviar_mensaje(canal, "20")
-
-      if ircmsg.find("edad") != -1:
-            obtener_mensaje(canal, ircms)
-
-
-
 
 
