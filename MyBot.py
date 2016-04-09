7d41aea56a0be55ff06fb93a08013e9087d8ba82
Verónica Expósito Cano <v.exposito.cano@gmail.com>
2016-04-10 00:18:51 +0200

    v 1.0
    Bot definido en clases:
    Guarda en la base de datos los mensajes capturados a partir de un
    patrón.

--- MyBot.py
+++ MyBot.py
@@ -1,199 +1,144 @@
-# -*- coding: cp1252 -*-
-#     =====================================================================================================================     #
-#     ==================================================== IMPORT =========================================================     #
-#     =====================================================================================================================     #
-
 import socket
 import time
-import string # módulo que contiene las secuencias comunes de caracteres ASCII
-import random # módulo que se ocupa de la generación aleatoria
+import string # modulo que contiene las secuencias comunes de caracteres ASCII
+import random # modulo que se ocupa de la generacion aleatoria
 import sqlite3
 
-#     =====================================================================================================================     #
-#     ============================================ DEFINICION DE VARIABLES ================================================     #
-#     =====================================================================================================================     #
 
-servidor    = "irc.irc-hispano.org"
-canal       = "#ircVero"
-nombre      = "__Aliiicia36"
-puerto      = 6667
-#ident       = "__Aliiicia"
-#realname    = "__Aliiicia"
-capturar    = False
-nombreNuevo = ['#Raquel20', '#Flor20', '#Carlota20', '#Mery20', '#Andrea20', '#Susi20',
-               '#Suri20', '#Moni20', '#Ami20', 'Marga20', 'Gracia20', 'Bea20',
-               'Andaluza20']
+class BotClases:    
+      def __init__(self, servidor, canal, nombre, puerto, capturar, patron):
+            self.irc          = None
+            self.servidor     = servidor
+            self.canal        = canal
+            self.nombre       = nombre
+            self.puerto       = puerto
+            self.capturar     = capturar
+            self.patron       = patron
 
-      
-#     =====================================================================================================================     #
-#     ============================================ DEFINICION DE FUNCIONES ================================================     #
-#     =====================================================================================================================     #
- 
-def respuesta_ping(canal, ircmsg):
-      if ircmsg.find("PING :") != -1:
-            respuesta_ping = ircmsg
-            respuesta_ping = respuesta_ping[respuesta_ping.find("PING :"):]
-            respuesta_ping = respuesta_ping.split("\n", 1 )
-            respuesta_ping = respuesta_ping[0].replace("I", "O", 1)
-            irc.send(respuesta_ping)
-            unirse_a_canal(canal)    
-            print "\n\r"
+      def respuesta_ping(self, canal, ircmsg):
+            if ircmsg.find("PING :") != -1:
+                  respuesta_ping = ircmsg
+                  respuesta_ping = respuesta_ping[respuesta_ping.find("PING :"):]
+                  respuesta_ping = respuesta_ping.split("\n", 1 )
+                  respuesta_ping = respuesta_ping[0].replace("I", "O", 1)
+                  self.irc.send(respuesta_ping)
+                  self.unirse_a_canal(self.canal)    
+                  print "\n\r"
         
-def enviar_mensaje(canal , ircmsg):
-      irc.send("PRIVMSG " + canal + " : " + ircmsg + "\n\r") 
-      
-      
-def unirse_a_canal(canal):
-      irc.send("JOIN " + canal + "\n\r")
+      def enviar_mensaje(self, canal, ircmsg):
+            self.irc.send("PRIVMSG " + self.canal + " : " + self.ircmsg + "\n\r")            
+          
+      def unirse_a_canal(self, canal):
+            self.irc.send("JOIN " + self.canal + "\n\r")
 
-def desconectarse(canal):
-      irc.send("QUIT " + canal + "\n\r")
-      
-def  obtener_nick(canal, ircmsg):
-      if ircmsg.find("PRIVMSG " + canal) != -1:
-            nick = ircmsg.split('!', 1 )
-            nick = nick[0].replace(":", "",1)
-            print(ircmsg + canal)
-            return nick
-      
-      
-def obtener_mensaje(canal, ircmsg):
-      if ircmsg.find("PRIVMSG " + canal) != -1:
-            mensaje = ircmsg.split(canal + ' ', 1 )
-            mensaje = mensaje[1].replace(":", " ",1)
-            return mensaje
- 
- 
-def mostrar_chat(canal, ircmsg):
-      print (obtener_nick(canal, ircmsg) + " : " + obtener_mensaje(canal, ircmsg))
-
-def existe_idev(idev):
-      print cursor.execute("SELECT IDEV FROM EVENTO WHERE IDEV == '" + idev + "'")
-      
-      if (cursor.fetchone())!= None:
-            return True
-      else:
-            return False
+      def desconectarse(self, canal):
+            self.irc.send("QUIT " + self.canal + "\n\r")
             
-      
+      def  obtener_nick(self, canal, ircmsg):
+            if ircmsg.find("PRIVMSG " + self.canal) != -1:
+                  nick = ircmsg.split('!', 1 )
+                  nick = nick[0].replace(":", "",1)
+                  print(ircmsg + self.canal)
+                  return nick            
+            
+      def obtener_mensaje(self, canal, ircmsg):
+            if ircmsg.find("PRIVMSG " + self.canal) != -1:
+                  mensaje = ircmsg.split(self.canal + ' ', 1 )
+                  mensaje = mensaje[1].replace(":", " ",1)
+                  return mensaje
+       
+      def mostrar_chat(self, canal, ircmsg):
+            print (self.obtener_nick(self.canal, ircmsg) + " : " + self.obtener_mensaje(self.canal, self.ircmsg))
 
-def generar_idev(canal,ircmsg):
-      probarDeNuevo = True
-      while probarDeNuevo:            
-            numerito = random.choice(range(100))
-            t1 = time.strftime('%H %M %S')
-            t1 = t1.replace(" ", "")
-            idev = 'BOT0' + t1 + str(numerito)
-            if existe_idev(idev) == False:
-                  probarDeNuevo = False
+      def existe_idev(self, idev):
+            print self.cursor.execute("SELECT IDEV FROM EVENTO WHERE IDEV == '" + idev + "'")            
+            if (self.cursor.fetchone())!= None:
+                  return True
             else:
-                  probarDeNuevo = True                        
-      return idev
-
-
-def analizar_canal(canal,ircmsg):
-      patron = ['wola', 'qtal']
-      global capturar
-      global cursor
-      global con
-      global idev
-      for i in patron:                  
-            if ((ircmsg.find(i)!= -1)):
-                  tiempo = time.strftime('%H:%M:%S / %d %b %y \n\r')
-                  print '================================ ENCONTRADO ==============================='+ "\n\r"
-                  print tiempo + "\n\r" + 'En el canal: ' + canal + "\n\r"
-                  print 'Se ha encontrado el patron: \''+ i + '\' \n\r'
-                  print '==========================================================================='+ "\n\r"
-                  # Escribir en un txt los mensajes caoturados
-                  capturado = open('capturado.txt', 'w')                 
-                  capturado.write(tiempo + '\n\r'  + 'Captura del canal: ' + canal + "\n\r")
-                  # Creamos un IDEV para relacionar las dos tablas con el mismo ID
-                  idev = generar_idev(canal,ircmsg)
-                  
-                  # Escribir datos en una BD
-                  capturar = True
-                  cursor.execute ("INSERT INTO EVENTO (IDEV, FECHA, CANAL, SERVIDOR, PATRON) VALUES ('" + idev + "' ,'" + tiempo + "' , '" + canal + "', '" + servidor +"', '" + i + "')")
-                  con.commit()
-
-    
+                  return False
                 
+      def generar_idev(self):
+            probarDeNuevo = True
+            while probarDeNuevo:            
+                  numerito    = random.choice(range(100))
+                  t1          = time.strftime('%H %M %S')
+                  t1          = t1.replace(" ", "")
+                  idev        = 'BOT0' + t1 + str(numerito)
+                  if self.existe_idev(idev) == False:
+                        probarDeNuevo = False
+                  else:
+                        probarDeNuevo = True                        
+            return idev
 
-def guardar_msg(canal, ircmsg):
-      usuario = obtener_nick(canal, ircmsg)
-      #print '--> ' + usuario + "     " + '--> '+ ircmsg + '\n\r'
-      capturado = open('capturado.txt', 'a')
-      capturado.write("\n\r" + '    ' + usuario + "\n\r" + '      '+ ircmsg + '\n\r')
-      capturado.close()
+      def analizar_canal(self, canal,ircmsg):
+            global capturar
+            global cursor
+            global con
+            global idev
 
-def guardar_bd(canal, ircmsg, idev):
-      idev
-      usuario = obtener_nick(canal, ircmsg)
-      tiempo = time.strftime('%H:%M:%S / %d %b %y \n\r')
-      con = sqlite3.connect('irc.db')
-      #con_bd.close()
-      cursor = con.cursor()
-      #cursor_agenda.close()
-      cursor.execute ("INSERT INTO MENSAJES (IDEV, FECHA, CANAL, SERVIDOR, USUARIO, MENSAJE) VALUES ('" + idev + "' ,'" + tiempo + "' , '" + canal + "', '" + servidor + "', '" + usuario +"', '" + ircmsg +"')")
-      con.commit()
+            for i in self.patron:                  
+                  if ((ircmsg.find(i)!= -1)):
+                        tiempo = time.strftime('%d %b %y / %H:%M:%S \n\r')
+                        print '================================ ENCONTRADO ==============================='+ "\n\r"
+                        print tiempo + "\n\r" + 'En el canal: ' + self.canal + "\n\r"
+                        print 'Se ha encontrado el patron: \''+ i + '\' \n\r'
+                        print '==========================================================================='+ "\n\r"
+                        # Escribir en un txt los mensajes caoturados
+                        capturado = open('capturado.txt', 'w')                 
+                        capturado.write(tiempo + '\n\r'  + 'Captura del canal: ' + self.canal + "\n\r")
+                        # Creamos un IDEV para relacionar las dos tablas con el mismo ID
+                        idev = self.generar_idev()                        
+                        # Escribir datos en una BD
+                        self.capturar = True
+                        self.cursor.execute ("INSERT INTO EVENTO (IDEV, FECHA, CANAL, SERVIDOR, PATRON) VALUES ('" + idev + "' ,'" + tiempo + "' , '" + self.canal + "', '" + self.servidor +"', '" + i + "')")
+                        self.con.commit()  
+                      
+      def guardar_msg(self, canal, ircmsg):
+            usuario     = obtener_nick(self.canal, self.ircmsg)  #print '--> ' + usuario + "     " + '--> '+ ircmsg + '\n\r'
+            capturado   = open('capturado.txt', 'a')
+            capturado.write("\n\r" + '    ' + usuario + "\n\r" + '      '+ self.ircmsg + '\n\r')
+            capturado.close()
 
+      def guardar_bd(self, canal, ircmsg, idev):
+            cursor           = self.cursor
+            con              = self.con
+            usuario          = self.obtener_nick(self.canal, ircmsg)
+            tiempo           = time.strftime('%H:%M:%S / %d %b %y \n\r')
+            con              = sqlite3.connect('irc.db')          
+            cursor           = con.cursor()  #con.close()
+            #print '--> ' + usuario + "     " + '--> '+ self.ircmsg + '\n\r'
+            cursor.execute ("INSERT INTO MENSAJES (IDEV, FECHA, CANAL, SERVIDOR, USUARIO, MENSAJE) VALUES ('" + self.idev + "' ,'" + tiempo + "' , '" + self.canal + "', '" + self.servidor + "', '" + usuario +"', '" + self.ircmsg +"')")
+            con.commit()
 
-      
-#     =====================================================================================================================     #
-#     ========================================= ESTABLECIMIENTO DE LA CONEXIÓN ============================================     #
-#     =====================================================================================================================     #
+      def conexion(self):            
+            self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.irc.connect((self.servidor, self.puerto))
+            self.irc.send("NICK " + self.nombre +"\r\n")
+            self.irc.send("USER " + self.nombre + " 0 * : " + self.nombre + "\r\n")
+            self.irc.send("JOIN : %s \r\n" % self.canal)
 
-           
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc.connect((servidor, puerto))
-#time.sleep(10)
-irc.send("NICK "+ nombre +"\r\n")
-irc.send("USER " + nombre + " 0 * : " + nombre + "\r\n")
-#time.sleep(10)
-irc.send("JOIN : %s \r\n" % canal)
+             
+            print "Conectando... "+"\n\r"
+            self.unirse_a_canal(self.canal)
+            time.sleep(4)
+            self.unirse_a_canal(self.canal)
+            print '================================ CONECTADO ================================' + "\n\r"
+            print "INFORMACIONDE LA SESION" +"\n\r" + time.strftime('%H:%M:%S / %d %b %y')+"\n\r"+ "Canal: " + self.canal + "\n\r"+ "Servidor: " + self.servidor + "\n\r"
+            print '===========================================================================' + "\n\r"
 
+            while 1:
+                  self.ircmsg     = self.irc.recv(512)  #print time.strftime('%H:%M:%S \n\r') + ircmsg  + "\n\r"
+                  self.con        = sqlite3.connect('irc.db')  #con_bd.close()
+                  self.cursor     = self.con.cursor()  #cursor.close()
+                  self.capturar   
+                  #respuesta_ping(canal, ircmsg)
 
-#irc.send("USER "+ nombre +" "+ nombre +" "+ nombre + "\r\n")
-#irc.send("NICK %s\r\n" % nombre)
-#irc.send("USER %s %s bla : %s\r\n" % (nombre, servidor, nombre))
+                  if ((self.ircmsg.find("PING") != -1)):            
+                        self.respuesta_ping(self.canal, self.ircmsg)
 
- 
-print "Conectando... "+"\n\r"
-unirse_a_canal(canal)
-time.sleep(4)
-unirse_a_canal(canal)
-print '================================ CONECTADO ================================' + "\n\r"
-#print "¡CONECTADO! " +"\n\r"
-print "INFORMACIÓNDE LA SESION" +"\n\r" + time.strftime('%H:%M:%S / %d %b %y')+"\n\r"+ "Canal: " + canal + "\n\r"+ "Servidor: " + servidor + "\n\r"
-print '===========================================================================' + "\n\r"
-#time.sleep(5)
-#desconectarse(canal)
-#print("¡Desconectado! ")
-#respuesta_ping(ircmsg,canal)
-
-#     =====================================================================================================================     #
-#     ================================================ TRABAJANDO =========================================================     #
-#     =====================================================================================================================     #
-
-while 1:
-      ircmsg = irc.recv(512)
-      #print time.strftime('%H:%M:%S \n\r') + ircmsg  + "\n\r"
-      con = sqlite3.connect('irc.db')
-      #con_bd.close()
-      cursor = con.cursor()
-      #cursor.close()
-      capturar
-      #respuesta_ping(canal, ircmsg)
-
-      if ((ircmsg.find("PING") != -1)):            
-            respuesta_ping(canal, ircmsg)
-
-      if ((ircmsg.find("PRIVMSG") != -1)):            
-            analizar_canal(canal,ircmsg)
-            if (capturar == True):
-                  #guardar_msg(canal,ircmsg)
-                  guardar_bd(canal, ircmsg, idev)
-
-
-
-
+                  if ((self.ircmsg.find("PRIVMSG") != -1)):            
+                        self.analizar_canal(self.canal, self.ircmsg)
+                        if (self.capturar == True):
+                              #guardar_msg(canal,ircmsg)
+                              self.guardar_bd(self.canal, self.ircmsg, self.idev)
 
---
+++ hebra.py
@@ -0,0 +1,36 @@
+import threading
+from BotClases import BotClases
+
+patron      = ['guapa', 'alguna', 'nena', 'mujer']
+servidor    = "irc.irc-hispano.org"
+canal       = "#Murcia"
+nombre      = "__Aliiicia36"
+puerto      = 6667
+capturar    = False
+
+patron1      = ['guapa', 'alguna', 'nena', 'mujer']
+servidor1    = "irc.irc-hispano.org"
+canal1       = "#Granada"
+nombre1      = "__Aliiicia"
+puerto1      = 6667
+capturar1    = False
+
+class HebraBot(threading.Thread):
+     def __init__(self, servidor, canal, nombre, puerto, capturar, patron):
+         super(HebraBot, self).__init__()
+         self.bot = BotClases(servidor, canal, nombre, puerto, capturar, patron);
+
+     def run(self):
+         self.bot.conexion()
+
+
+hebraBot1 = HebraBot(servidor, canal, nombre, puerto, capturar, patron)
+hebraBot2 = HebraBot(servidor1, canal1, nombre1, puerto1, capturar1, patron1)
+
+
+hebraBot1.start()
+hebraBot2.start()
+
+
+
+
